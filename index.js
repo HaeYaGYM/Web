3fc1b0161d5fe5293c363646fe016e3f70737d64
kh4b1 <lhj9242@naver.com>
2022-10-15 12:21:25 +0900

    update

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 const express = require("express"),
-  path = require("path"),
-  fs = require("fs")
+  fs = require("fs"),
+  authrouter = require("./routes/Auth"),
+  boardrouter = require("./routes/Board"),
+  calendarrouter = require("./routes/Calendar")
 
 const app = express()
+
 app.set('view engine', 'html')
 app.use(express.static('public'))
 app.use(express.static('views'))
+
+
+app.use("/auth", authrouter)
+app.use("/board", boardrouter)
+app.use("/calendar", calendarrouter)
+
 app.get("/", (req, res) => {
   fs.readFile(`./views/index.html`, (error, data) => {
       if (error) {
@@ -16,6 +25,8 @@ app.get("/", (req, res) => {
       res.end(data);
   });
 });
+
+
 
 const port = 8000
 app.listen(port, () => {
