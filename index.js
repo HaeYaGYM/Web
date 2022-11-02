a3be765bbd4fb89d8c05a34b6a64d04b7725ff00
Kh4b1 <lhj9242@naver.com>
2022-11-03 05:17:24 +0900

    session

--- index.js
+++ index.js
@@ -1,32 +1,45 @@
 const express = require("express"),
   fs = require("fs"),
-  authrouter = require("./routes/Auth"),
-  boardrouter = require("./routes/Board"),
-  calendarrouter = require("./routes/Calendar")
+  session = require('express-session'),
+  filestore = require('session-file-store')(session)
+
+
 
 //const database = require("./config/database") 
 const app = express()
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
 
-app.set('view engine', 'html')
+app.set('view engine', 'ejs')
 app.use(express.static('public'))
 app.use(express.static('views'))
 
+app.use(session({
+  secret:'hyg',
+  resave: false,
+  saveUninitialized: true,
+  store:new filestore()
+}))
+
+const authrouter = require("./routes/Auth"),
+  boardrouter = require("./routes/Board"),
+  calendarrouter = require("./routes/Calendar")
 
 app.use("/auth", authrouter)
 app.use("/board", boardrouter)
 app.use("/calendar", calendarrouter)
 
 app.get("/", (req, res) => {
-  fs.readFile(`views/index.html`, (error, data) => {
-      if (error) {
-          console.log(error);
-          return res.status(500).sendFile("./views/404.html");
-      }
-      res.render
-      res.writeHead(200, { "Content-Type": "text/html" });
-      res.end(data);
-  });
-});
+  if(req.session.isOwner == true){
+    res.render('index',{
+        isOwner : req.session.isOwner,
+        nick : req.session.nick
+    })
+}else{
+    res.render('index',{
+        isOwner : false
+    })
+}})
 
 
 
--- routes/Auth.js
+++ routes/Auth.js
@@ -2,42 +2,53 @@ const express = require("express")
 const router = express.Router()
 const fs = require("fs")
 
-    
-router.get("/sign_in", (req,res)=>
-        fs.readFile(`views/sign_in.html`, (error, data) => {
-            if (error) {
-                console.log(error)
-                return res.status(500).send("<h1>500 Error</h1>")
-            }
-            res.writeHead(200, { "Content-Type": "text/html" })
-            res.end(data);
-        }))
+var auth = {
+    email : '111@111',
+    password : '1111',
+    nick : '1'
+}
 
-router.post('/sign_in', (req,res) =>{
-            var email = req.email
-            var pw = req.pw
-            
+router.get("/sign_in", (req,res)=>{
+    res.render('sign_in')})
+
+router.post('/sign_in_process', (req,res) =>{
+    var email = req.body.email
+    var password = req.body.password
+    console.log(req.session)
+    console.log(req.body.email)       
+    if (email === auth.email && password === auth.password){
+                req.session.isOwner = true
+                req.session.nickname = auth.nick
+                console.log('Welcome')
+                res.redirect('/')
+            } else (
+                res.send('Who')
+            )
         })
 
 router.get("/sign_up", (req,res)=>
-        fs.readFile(`views/sign_up.html`, (error, data) => {
-            if (error) {
-                console.log(error)
-                return res.status(500).send("<h1>500 Error</h1>")
-            }
-            res.writeHead(200, { "Content-Type": "text/html" })
-            res.end(data);
-        }))
+        res.render('sign_up'))
 
-router.post('/sign_up', (req,res) =>{
-        var email = req.email
-        var pw = req.pw
-        var pw2 = req.pw2
-        var name = req.name
-        var nick = req.nick
-        var weight = req.weight
-        var height = req.height
-        var birth = req.birth
+
+router.post('/sign_up_process', (req,res) =>{
+        var email = req.body.email
+        var pw = req.body.pw
+        var pw2 = req.body.pw2
+        var name = req.body.name
+        var nick = req.body.nick
+        var weight = req.body.weight
+        var height = req.body.height
+        var birth = req.body.birth
+        if(pw !== pw2){
+            res.write("<script>alert('비밀번호가 같지 않습니다')</script>")
+            res.write("<script>window.location=\"../views/sign_up\"</script>")
+        }
     })
+
+router.get('/logout', (req,res)=>{
+    req.session.destroy(function(error){
+res.redirect('/')
+    }
+)})    
         
 module.exports = router
