1c97ef644103a678a665cb030bcbbc90715fecb8
Kh4b1 <lhj9242@naver.com>
2022-11-08 07:39:21 +0900

    si

--- routes/Board.js
+++ routes/Board.js
@@ -1,6 +1,14 @@
 const express = require("express");
 const router = express.Router();
-const fs = require("fs")
-router.get("/list", (req,res)=>
-        res.render('board_list'))
+router.get("/", (req,res)=>{
+        if(req.session.isOwner == true){
+        res.render('board_list',{
+            isOwner : req.session.isOwner,
+            email : req.session.email
+        })
+    }else{
+        res.render('board_list',{
+            isOwner : false
+        })
+    }})
 module.exports = router
